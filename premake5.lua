d65409dfb2d93810dac63d0675f946c0a463caab
Cankar001 <info.can-karka@web.de>
2021-05-09 02:07:16 +0200

    Added new binaries and premake script

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+project "assimp"
+    kind "SharedLib"
+    language "C++"
+    staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/assimp/**.h",
+		"include/assimp/**.hpp",
+		"include/assimp/**.inl"
+    }
+    
+	filter "system:windows"
+        systemversion "latest"
+
+		defines 
+		{ 
+            "_CRT_SECURE_NO_WARNINGS"
+		}
+
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "on"
+			
+			
